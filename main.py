3338b5819d117af712b4303a75b496facc1b1104
Otabek Tursunov <otabecktursunov@gmail.com>
2023-12-17 21:21:14 +0500

    project created, configure BOT settings

---
+++ main.py
@@ -0,0 +1,36 @@
+import asyncio
+import logging
+import sys
+from os import getenv
+
+from aiogram import Bot, Dispatcher, Router, types
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+
+TOKEN = getenv("BOT_TOKEN")
+
+dp = Dispatcher()
+
+
+@dp.message(CommandStart())
+async def command_start_handler(message: Message) -> None:
+    await message.answer(f"Hello, {message.from_user.full_name}!")
+
+
+@dp.message()
+async def wikipedia_handler(message: types.Message) -> None:
+    try:
+        await message.send_copy(chat_id=message.chat.id)
+    except TypeError:
+        await message.answer("Nice try!")
+
+
+async def main() -> None:
+    bot = Bot(TOKEN, parse_mode=ParseMode.HTML)
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(main())
